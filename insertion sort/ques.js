ef5d0decc2b57461f71bce7e52f48359340e68a3
363tyagiayush-png <363tyagiayush@gmail.com>
2026-02-10 22:14:11 +0530

    add bubble sort, insertion sort, and selection sort implementations

---
+++ insertion sort/ques.js
@@ -0,0 +1,41 @@
+// insertion sort is a simple algorithm that builds the final sorted array one element at a time by inserting each element into 
+//its correct position with in the already sorted position of the array.
+
+//we have to assume that the first element is sorted
+//
+
+
+// function insertionSort(arr){
+// // we assume that first element is sorted (arr[0])
+
+//    for(let i=1; i<arr.length; i++){
+//     let current = arr[i]; //element to insert
+//     let j = i-1; //bound of the sorted portion 
+
+//     while(j>=0 && arr[j]>current){
+//         arr[j+1]=arr[j]
+//         j--;
+//     }
+//     arr[j+1]=current;
+//    }
+//    return arr
+// }
+// console.log(insertionSort([5,2,4,6,1,3]))
+
+
+function insertionSort(arr){
+// we assume that first element is sorted (arr[0])
+
+   for(let i=1; i<arr.length; i++){
+    let current = arr[i]; //element to insert
+    let j = i-1; //bound of the sorted portion 
+
+    while(j>=0 && arr[j]>current){
+        arr[j+1]=arr[j]
+        j--;
+    }
+    arr[j+1]=current;
+   }
+   return arr
+}
+   console.log(insertionSort([5,2,4,6,1,3]))
