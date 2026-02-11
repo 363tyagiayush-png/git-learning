162c0372e00c584ab6cfca0b35a46986f7138120
363tyagiayush-png <363tyagiayush@gmail.com>
2026-02-11 17:44:18 +0530

    new change

---
+++ praticeques/function.js
@@ -0,0 +1,78 @@
+// function sayMyName(){
+//     console.log("love Bubber")
+// }
+// sayMyName();
+
+// function printCounting(){
+//     for(let i=0; i<=10; i++){
+//         console.log(i);
+//     }
+// }
+// printCounting();
+
+
+// function printNumber(num){
+//     console.log("printing Number:", num);
+// }
+// printNumber(5);
+
+// function getAverage(num1 ,num2){
+//     let avg = (num1+num2)/2;
+//     console.log("Average:",avg);
+// }
+// getAverage(3,70);
+
+//return function
+// function getSum(a,b,c){
+//     let Sum = a+b+c
+//     return Sum;
+// }
+// let ans = getSum(1,2,3);
+// console.log("Printing Sum:" , ans)
+
+// arrow function
+// let getExp =(x,y) => {
+//     let ans = x**y;
+//     return ans;
+// }
+// console.log(getExp(2,4));
+
+
+
+// binarySearch   
+
+let arr = [1,2,3,4,5,6,7,8
+    
+]
+
+function binarySearch(arr) {
+    let target = 8
+    let start = 0
+    let last = arr.length - 1
+
+    while (start <= last) {
+
+        let mid = Math.floor((start + last) / 2)
+console.log("hello");
+
+        if (target == arr[mid]) {
+            return arr[mid]
+        } 
+        else if (target > arr[mid]) {
+            start = mid + 1
+        } 
+        else {
+            last = mid - 1
+        }
+    }
+ 
+}
+
+console.log(binarySearch(arr));
+
+
+
+
+
+
+
